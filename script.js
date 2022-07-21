d73dd59ce60ab322dee266a969f2ea4cd2b1a2d8
Matt Walker <71237406+MattWalker1@users.noreply.github.com>
2022-07-20 21:12:01 -0600

    tweak to file structure

---
+++ script.js
@@ -0,0 +1,5 @@
+const generateRequestCallback = function (callback) {
+    const callback = document.createElement('div')
+    const numberbox = document.createElement('input')
+
+    return callback };
